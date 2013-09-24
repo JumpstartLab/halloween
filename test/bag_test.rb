8a9d2fa21364733e7451df3e5a547c4cd897e43b
Katrina Owen <katrina.owen@gmail.com>
2013-09-24 16:52:37 -0600

    Improve bag tests

--- test/bag_test.rb
+++ test/bag_test.rb
@@ -5,20 +5,31 @@ require './lib/bag'
 require './lib/candy'
 
 class BagTest < Minitest::Test
-  def test_bag_is_empty
+  def test_a_new_bag_is_empty
     assert Bag.new.empty?
   end
 
-  def test_empty_bag_has_zero_candies
+  def test_count_the_candies_in_an_empty_bag
     skip
     assert_equal 0, Bag.new.count
   end
 
-  def test_bag_takes_candy
+  def test_empty_bag_has_no_candies
+    skip
+    assert_equal [], Bag.new.candies
+  end
+
+  def test_put_candy_in_the_bag
     skip
     bag = Bag.new
-    candy = Candy.new("Nerds")
+    candy = Candy.new("Sour frogs")
     bag << candy
+    assert_equal [candy], bag.candies
+  end
+
+  def test_a_bag_with_candies_is_not_empty
+    bag = Bag.new
+    bag << Candy.new("Nerds")
     refute bag.empty?
   end
 
@@ -29,19 +40,10 @@ class BagTest < Minitest::Test
     assert_equal 1, bag.count
   end
 
-  def test_bag_contains_candies
-    skip
-    bag = Bag.new
-    candy = Candy.new("Sour frogs")
-    bag << candy
-    assert_equal [candy], bag.candies
-  end
-
   def test_bag_contains_candies_and_candies_have_a_type
     skip
     bag = Bag.new
-    candy = Candy.new("Hershey's Kisses")
-    bag << candy
+    bag << Candy.new("Hershey's Kisses")
     # You usually don't want to chain a bunch of different
     # types of things together like this.
     # We'll talk about it more in a few weeks.
